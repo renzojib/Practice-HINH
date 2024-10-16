4ff896f799301164c5e0bd81c6fb2750370892a3
pjancura <jancura.paul@gmail.com>
2024-10-16 15:03:05 -0400

    creates the route folders and index.tsx's, updates (tabs)/_layout.tsx to use the new routes

---
+++ app/(tabs)/emergencyContacts/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const EmergencyContacts = () => {
+    return (
+        <View>
+            <Text>Hello from E-Contacts</Text>
+        </View>
+    )
+}
+
+export default EmergencyContacts
