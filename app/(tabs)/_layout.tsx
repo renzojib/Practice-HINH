3129506c9179e8a848f3dc5d34067103a62e18d5
pjancura <jancura.paul@gmail.com>
2024-10-16 15:31:19 -0400

    updates (tabs)/_layout.tsx with an initialRouteName prop

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,6 +15,7 @@ export default function TabLayout() {
 
   return (
     <Tabs
+      initialRouteName='/helpMe/index'
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
