9cd9f9caeafea15b58a2f68381d29c61a95391fa
ccolkereybel <163072600+ccolkereybel@users.noreply.github.com>
2024-10-16 18:49:43 -0400

    emergency contacts

--- app/(tabs)/helpMe/index.tsx
+++ app/(tabs)/helpMe/index.tsx
@@ -1,11 +1,53 @@
-import { Text, View } from "react-native"
+import React from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  KeyboardAvoidingView,
+  View,
+  Text,
+} from "react-native";
+import {
+  ScrollView,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+
+import MyForm from "@/components/form";
+import { FormDataProvider } from "@/components/FormDataContext";
 
 const HelpMe = () => {
-    return (
-        <View>
-            <Text>Hello from Help Me in (Tabs)</Text>
-        </View>
-    )
-}
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <ScrollView>
+          <SafeAreaView>
+            <FormDataProvider>
+              <MyForm />
+            </FormDataProvider>
+          </SafeAreaView>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </GestureHandlerRootView>
+  );
+};
 
-export default HelpMe
+export default HelpMe;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+});
