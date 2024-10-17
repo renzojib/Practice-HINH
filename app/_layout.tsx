71cbd382eea7fb652b6af948e3de735bd51ffaad
pjancura <jancura.paul@gmail.com>
2024-10-17 18:18:30 -0400

    hides addContacts from tabBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout() {
       <Header></Header>
       </SafeAreaView>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
           <Stack.Screen name="+not-found" />
         </Stack>
     </ThemeProvider>
